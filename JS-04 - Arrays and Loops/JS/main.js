cb4f14c67356359e1f3894e4b7f72ea3dde5c297
AGIG <adriana.ibarra.lae@gmail.com>
2023-03-27 16:54:19 -0600

    Se agrega parte del ejercicio 3

--- JS-04 - Arrays and Loops/JS/main.js
+++ JS-04 - Arrays and Loops/JS/main.js
@@ -15,6 +15,19 @@ console.log("******* Ejercicio 1 Parte 3 *******")
     console.log(people);
 
 console.log("******* Ejercicio 2 *******")
+    
 
+console.log("******* Ejercicio 3 parte 1 *******")
+    let valor=100;    
+    do {
+    console.log(valor);
+    valor-=.5;
+    } while (valor>0);
+    console.log(valor);
 
-console.log("******* Ejercicio 3 *******")
+    console.log("******* Ejercicio 3 parte 2 *******")
+       
+
+    console.log("******* Ejercicio 3 parte 3 *******")
+
+    console.log("******* Ejercicio 3 parte 4 *******")
