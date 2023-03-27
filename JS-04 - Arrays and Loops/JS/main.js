6bb4d2629f019a163e02ed867a4cf3b58facffcb
AGIG <adriana.ibarra.lae@gmail.com>
2023-03-27 16:36:18 -0600

    Se agrega ejercicio 1

---
+++ JS-04 - Arrays and Loops/JS/main.js
@@ -0,0 +1,20 @@
+console.log("******* Ejercicio 1 Parte 1 *******")
+
+    let people=["Sofía","David","Juan"]
+    /* people.sort(); */
+    console.log(people);
+
+console.log("******* Ejercicio 1 Parte 2 *******")
+    people.push("Sara","Agustín");
+    people.shift();
+    console.log(people);
+
+console.log("******* Ejercicio 1 Parte 3 *******")
+    people.splice(1,0,"Renata");
+    people.push("Elena");
+    console.log(people);
+
+console.log("******* Ejercicio 2 *******")
+
+
+console.log("******* Ejercicio 3 *******")
